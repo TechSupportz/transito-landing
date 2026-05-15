2c772d323ec151b6231b04f660423175e1215cab
Nitish <tnitish654@gmail.com>
2026-05-15 19:13:03 +0800

    🛠️ Migrate to tailwind v4 🛠️ Update prettier config ⚡ Update img to Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
 
 const googlePlayUrl =
-    "https://play.google.com/store/apps/details?id=com.tnitish.transito";
+    "https://play.google.com/store/apps/details?id=com.tnitish.transito"
 const appStoreUrl =
-    "https://apps.apple.com/sg/app/transito-sg-bus-timings/id6464025035";
+    "https://apps.apple.com/sg/app/transito-sg-bus-timings/id6464025035"
 const privacyUrl =
-    "https://tnitish.notion.site/Transito-Privacy-Policy-1411b1e8456d4b4d83a5147ed9dbf6c0?pvs=4";
-const feedbackUrl = "https://forms.gle/EFWkSiVfYzsGeTjn8";
+    "https://tnitish.notion.site/Transito-Privacy-Policy-1411b1e8456d4b4d83a5147ed9dbf6c0?pvs=4"
+const feedbackUrl = "https://forms.gle/EFWkSiVfYzsGeTjn8"
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,4 +20,7 @@ export default defineConfig({
         "/privacy": privacyUrl,
         "/feedback": feedbackUrl,
     },
-});
+    vite: {
+        plugins: [tailwindcss()],
+    },
+})
